d6e73e9c04a69ccdc2ad273cd7b0e165fdd12b63
Tanne55 <ngtanpqkg2020@gmail.com>
2026-05-27 15:07:45 +0700

    feat: BT1_B3_ss4_lab

--- lab_practical/session_04_react_basics/my-app/src/App.jsx
+++ lab_practical/session_04_react_basics/my-app/src/App.jsx
@@ -3,17 +3,34 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import AndDemo from '../components/AndDemo'
-import ListRendering from '../components/ListRendering'
+import ProductCard from '../components/ProductCard'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const products = [
+    { id: 1, name: "iPhone 15", price: "25.000.000", image: "https://via.placeholder.com/200" },
+    { id: 2, name: "Samsung S24", price: "22.000.000", image: "https://via.placeholder.com/200" },
+    { id: 3, name: "Xiaomi 14", price: "15.000.000", image: "https://via.placeholder.com/200" },
+    { id: 4, name: "Xiaomi 14", price: "15.000.000", image: "https://via.placeholder.com/200" },
+    { id: 5, name: "Xiaomi 14", price: "15.000.000", image: "https://via.placeholder.com/200" },
+    { id: 6, name: "Xiaomi 14", price: "15.000.000", image: "https://via.placeholder.com/200" }
+  ];
 
   return (
-    <>
-      <ListRendering />
-    </>
-  )
+    <div>
+      <h1 style={{ textAlign: "center" }}>Cửa hàng điện thoại</h1>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        {products.map(product => (
+          <ProductCard
+            key={product.id}
+            name={product.name}
+            price={product.price}
+            image={product.image}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App
